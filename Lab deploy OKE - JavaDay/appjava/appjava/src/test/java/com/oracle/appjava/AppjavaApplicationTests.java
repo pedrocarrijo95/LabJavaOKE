44fc1773a2c6f3555489b33a661cdd516f6bc63a
Pedro Carrijo <55897093+pedrocarrijo95@users.noreply.github.com>
2023-05-19 18:19:16 -0300

    Add files via upload

---
+++ Lab deploy OKE - JavaDay/appjava/appjava/src/test/java/com/oracle/appjava/AppjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.appjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
